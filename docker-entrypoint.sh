765d49c98041881ac9252110a7ee6343b2db00ae
Alejandro Merino <alejocangrejo1404@gmail.com>
2026-03-16 23:43:26 -0600

    fix: fixed artisan command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 set -e
 
-# Generar APP_KEY si no está definida o está vacía
+cd /var/www/html
+
+# Asegurar APP_KEY en .env (Laravel la lee al cargar el archivo)
 if [ -z "$APP_KEY" ]; then
   APP_KEY=$(php -r "echo 'base64:'.base64_encode(random_bytes(32));")
-  echo "APP_KEY=$APP_KEY" >> /var/www/html/.env
   export APP_KEY="$APP_KEY"
 fi
+
+# Escribir APP_KEY en .env para que Laravel la encuentre
+php -r "
+\$file = '/var/www/html/.env';
+\$content = file_exists(\$file) ? file_get_contents(\$file) : '';
+\$key = getenv('APP_KEY');
+if (preg_match('/^APP_KEY=/m', \$content)) {
+  \$content = preg_replace('/^APP_KEY=.*/m', 'APP_KEY=' . \$key, \$content);
+} else {
+  \$content = trim(\$content) . \"\nAPP_KEY=\$key\n\";
+}
+file_put_contents(\$file, \$content);
+"
 
 php artisan config:clear
 php artisan migrate --force
