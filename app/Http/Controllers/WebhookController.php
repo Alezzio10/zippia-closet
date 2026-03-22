56ddf9ed52f02921ea59b65eb3af25f26826ad0f
Alejandro Merino <alejocangrejo1404@gmail.com>
2026-03-21 20:30:40 -0600

    fix webhook wompi

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -9,43 +9,11 @@ use App\Models\Pago;
 class WebhookController extends Controller
 {
     public function wompi(Request $request)
-{
-    \Log::info('🔥 WEBHOOK Wompi', $request->all());
-
-    $transaction = $request->input('data.transaction');
-
-    if (!$transaction) {
+    {
+        \Log::info('🔥 WEBHOOK LLEGO', $request->all());
+    
         return response()->json(['ok' => true], 200);
     }
-
-    $status = $transaction['status'] ?? null;
-    $reference = $transaction['reference'] ?? null;
-
-    if (!$reference) {
-        return response()->json(['ok' => true], 200);
-    }
-
-    // Ejemplo: pedido_5 → 5
-    $pagoId = str_replace('pedido_', '', $reference);
-
-    $pago = \App\Models\Pago::with('pedido')->find($pagoId);
-
-    if (!$pago) {
-        return response()->json(['ok' => true], 200);
-    }
-
-    $nuevoEstado = ($status === 'APPROVED') ? 'Completado' : 'Fallida';
-
-    $pago->estado = $nuevoEstado;
-    $pago->save();
-
-    if ($pago->pedido) {
-        $pago->pedido->estado = ($nuevoEstado === 'Completado') ? 'PAGADO' : 'PENDIENTE';
-        $pago->pedido->save();
-    }
-
-    return response()->json(['ok' => true], 200);
-}
 }
 
 
